f610b14433375980044aa169a5852730ccb08553
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-06-12 12:47:21 +0530

    blog done

--- assets/js/eventData.js
+++ assets/js/eventData.js
@@ -1,37 +1,37 @@
 eventData=[
 
     {
-        id:"1",
+        id:"0",
         imgSrc:"assets/images/event/pic1.jpg",
-        title:"Education Autumn Tour 2019",
-        content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
+        title:"0Education Autumn Tour 2019",
+        content:"0Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
         fullDate:"20/06/2021",
         date:"26",
         month:"20/06/2021"
     },
     {
-        id:"3",
-        imgSrc:"assets/images/event/pic2.jpg",
-        title:"Education Autumn Tour 2019",
-        content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
-        fullDate:"20/06/2021",
-        date:"26",
-        month:"20/06/2021"
-    },
-    {
-        id:"4",
-        imgSrc:"assets/images/event/pic3.jpg",
-        title:"Education Autumn Tour 2019",
-        content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
+        id:"1",
+        imgSrc:"assets/images/event/pic4.jpg",
+        title:"1Education Autumn Tour 2019",
+        content:"1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
         fullDate:"20/06/2021",
         date:"26",
         month:"20/06/2021"
     },
     {
         id:"2",
-        imgSrc:"assets/images/event/pic4.jpg",
-        title:"Education Autumn Tour 2019",
-        content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
+        imgSrc:"assets/images/event/pic2.jpg",
+        title:"2Education Autumn Tour 2019",
+        content:"2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
+        fullDate:"20/06/2021",
+        date:"26",
+        month:"20/06/2021"
+    },
+    {
+        id:"3",
+        imgSrc:"assets/images/event/pic3.jpg",
+        title:"2Education Autumn Tour 2019",
+        content:"2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
         fullDate:"20/06/2021",
         date:"26",
         month:"20/06/2021"
@@ -39,43 +39,3 @@ eventData=[
 ]
 
 
-var allEvents=""
-
-function generateEventBlock({date,month,fullDate,content,title,imgSrc}){
-    return  `
-
-    <li class="action-card col-lg-6 col-md-6 col-sm-12 happening">
-                                    <div class="event-bx m-b30">
-                                        <div class="action-box">
-                                            <img src="${imgSrc}" alt="">
-                                        </div>
-                                        <div class="info-bx d-flex">
-                                            <div>
-                                                <div class="event-time">
-                                                    <div class="event-date">${date}</div>
-                                                    <div class="event-month">${month}</div>
-                                                </div>
-                                            </div>
-                                            <div class="event-info">
-                                                <h4 class="event-title"><a href="#">${title}</a></h4>
-                                                <ul class="media-post">
-                                                    <li><a href="#"><i class="fa fa-clock-o"></i> ${fullDate}</a></li>
-                                                   
-                                                </ul>
-                                                <p>${content.substring(0,145)}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-    `
-}
-
-for(var i=0;i<eventData.length;i++){
-
-    allEvents+=generateEventBlock(eventData[i])
-}
-
-
-
-
-document.getElementById("masonry").innerHTML=allEvents
