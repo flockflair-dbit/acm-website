c2cdbb3d55df32bde2c22c923bb4ecc2a45471dc
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-06-12 13:36:21 +0530

    added sample event

--- assets/js/eventData.js
+++ assets/js/eventData.js
@@ -2,9 +2,28 @@ eventData=[
 
     {
         id:"0",
-        imgSrc:"assets/images/event/pic1.jpg",
-        title:"0Education Autumn Tour 2019",
-        content:"0Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
+        imgSrc:"assets/images/event/flashposter.jpg",
+        title:"Flash Workshop",
+        content:`<div class="post-content clearfix"><p><span>In order to strengthen the designing and animation skills of our students, a Flash Workshop was scheduled on 30th July 2020. Mr. Hariharan Iyer was selected as the mentor for this workshop due to his Designing skills.</span><br><span id="more-5044"></span></p>
+        <p id="viewer-a8vf5" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr">The workshop started of with Mrs. Mayura beginning of with a small message. Grejo Joby then tool over the platform and Introduced the basic things related to Flash, its history in the animation field and the ups and downs its facing. He ended with clearing out the question which was in a sense in everyone’s mind ‘Why to learn Flash when there are better softwares?’ </span></p>
+        <div id="viewer-dk8be" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr">
+        <p id="viewer-7het5" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr">Once the students were accustomed to knowing about Flash and its advantages Mr. Hariharan took the lead and started of with the Practical session. In the beginning he went over the basic tools and functions of Flash. As he advanced in the topic, methods such as animation effect, motion effects, shape effects, motion twins,etc were taught to the student. Once everyone had a idea on how to proceed in a animation world, Mr. Hariharan gave a example of one of his projects. A doubt session was put up for all those who had a questions to ask of.</span><span class="vkIF2 public-DraftStyleDefault-ltr"></span></p>
+        <p id="viewer-61vf7" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr"> Mrs. Mayura ended the workshop asking for a feedback for the workshop and a message.</span></p>
+        </div>
+        <p><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2019/01/flashposter.jpg" alt="" width="578" height="325" class="alignnone wp-image-19189 " srcset="https://dbit.acm.org/wp-content/uploads/2019/01/flashposter.jpg 1556w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-1536x864.jpg 1536w" sizes="(max-width: 578px) 100vw, 578px"></p>
+        <p></p>
+
+        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1024x576.jpg" alt="" class="wp-image-19190" width="579" height="325" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1536x864.jpg 1536w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1.jpg 1556w" sizes="(max-width: 579px) 100vw, 579px"></figure>
+
+        <p></p>
+
+        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1024x576.jpg" alt="" class="wp-image-19191" width="576" height="324" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1536x864.jpg 1536w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2.jpg 1556w" sizes="(max-width: 576px) 100vw, 576px"></figure>
+
+        <p></p>
+
+        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1.jpg" alt="" class="wp-image-19192" width="576" height="264" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1.jpg 807w, https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1-300x138.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1-768x353.jpg 768w" sizes="(max-width: 576px) 100vw, 576px"></figure>
+        <p></p>
+        </div>`,
         fullDate:"20/06/2021",
         date:"26",
         month:"20/06/2021"
