cd03a057fccf0fad5c4511b23dbb046ff3fae54d
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-06-13 15:46:14 +0530

    fetching blogs from server

--- assets/js/eventData.js
+++ assets/js/eventData.js
@@ -1,72 +1,46 @@
 eventData=[
 
-    {
-        id:"0",
-        imgSrc:"assets/images/event/flashposter.jpg",
-        title:"Flash Workshop",
-        content:`<div class="post-content clearfix"><p><span>In order to strengthen the designing and animation skills of our students, a Flash Workshop was scheduled on 30th July 2020. Mr. Hariharan Iyer was selected as the mentor for this workshop due to his Designing skills.</span><br><span id="more-5044"></span></p>
-        <p id="viewer-a8vf5" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr">The workshop started of with Mrs. Mayura beginning of with a small message. Grejo Joby then tool over the platform and Introduced the basic things related to Flash, its history in the animation field and the ups and downs its facing. He ended with clearing out the question which was in a sense in everyone’s mind ‘Why to learn Flash when there are better softwares?’ </span></p>
-        <div id="viewer-dk8be" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr">
-        <p id="viewer-7het5" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr">Once the students were accustomed to knowing about Flash and its advantages Mr. Hariharan took the lead and started of with the Practical session. In the beginning he went over the basic tools and functions of Flash. As he advanced in the topic, methods such as animation effect, motion effects, shape effects, motion twins,etc were taught to the student. Once everyone had a idea on how to proceed in a animation world, Mr. Hariharan gave a example of one of his projects. A doubt session was put up for all those who had a questions to ask of.</span><span class="vkIF2 public-DraftStyleDefault-ltr"></span></p>
-        <p id="viewer-61vf7" class="XzvDs _208Ie ljrnk blog-post-text-font blog-post-text-color _2QAo- _25MYV _1Fao9 ljrnk public-DraftStyleDefault-block-depth0 public-DraftStyleDefault-text-ltr"><span class="vkIF2 public-DraftStyleDefault-ltr"> Mrs. Mayura ended the workshop asking for a feedback for the workshop and a message.</span></p>
-        </div>
-        <p><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2019/01/flashposter.jpg" alt="" width="578" height="325" class="alignnone wp-image-19189 " srcset="https://dbit.acm.org/wp-content/uploads/2019/01/flashposter.jpg 1556w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2019/01/flashposter-1536x864.jpg 1536w" sizes="(max-width: 578px) 100vw, 578px"></p>
-        <p></p>
-
-        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1024x576.jpg" alt="" class="wp-image-19190" width="579" height="325" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1-1536x864.jpg 1536w, https://dbit.acm.org/wp-content/uploads/2021/01/flash1.jpg 1556w" sizes="(max-width: 579px) 100vw, 579px"></figure>
-
-        <p></p>
-
-        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1024x576.jpg" alt="" class="wp-image-19191" width="576" height="324" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1024x576.jpg 1024w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-300x169.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-768x432.jpg 768w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2-1536x864.jpg 1536w, https://dbit.acm.org/wp-content/uploads/2021/01/flash2.jpg 1556w" sizes="(max-width: 576px) 100vw, 576px"></figure>
-
-        <p></p>
-
-        <figure class="wp-block-image size-large is-resized"><img loading="lazy" src="https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1.jpg" alt="" class="wp-image-19192" width="576" height="264" srcset="https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1.jpg 807w, https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1-300x138.jpg 300w, https://dbit.acm.org/wp-content/uploads/2021/01/flashfeedback1-768x353.jpg 768w" sizes="(max-width: 576px) 100vw, 576px"></figure>
-        <p></p>
-        </div>`,
-        fullDate:"20/06/2021",
-        date:"26",
-        month:"20/06/2021"
-    },
-    {
-        id:"1",
-        imgSrc:"assets/images/event/pic4.jpg",
-        title:"C / C++ Workshop",
-        content:`
-        <p>In an attempt of increasing the knowledge of our college students, ACM DBIT organized the C/C++ workshop, which was held on 03rd September, 2020 from 11AM to 4PM. The main aim of this workshop was to brush up the C/C++ language coding of the students and also to make them ready for the future competitive environments and interviews.</p>
-
-
-
-<p>The workshop started of with Manasi Anantpurkar giving a brief about the importance of C/C++ language in today’s world, its use in interviews as a base for coders to get into jobs and also its application in various things. Followed by Hayden Cordeiro who started off with basic C/C++ syntax, which IDEs are preferred for both Windows and Linus. As time went complexity of the language increased. The participants were given a open for all chat box, wherein at any point of time they could stop the mentor and ask a doubt.</p>
-
+    // {
+    //     id:"0",
+    //     imgSrc:"assets/images/event/flashposter.jpg",
+    //     title:"Flash Workshop",
+    //     slug:"1",
+    //     content:``,
+    //     fullDate:"20/06/2021",
+    //     date:"26",
+    //     month:"20/06/2021"
+    // },
  
-<p>Later, Pakshal Ranawat took the next phase of the workshop and taught the remaining parts of C/C++ language which was the more advanced use of it. Then came the theory part which was handled by Grejo Joby, followed by Hayden Cordeiro who explained the code of a sample project and explained the proper application of this language with the help of the examples. Pakshal Ranawat explained different output questions which are usually asked in exams and interviews. A last and final doubt session was conducted wherein all the participants enthusiastically came up with doubts and problems and our group of resource persons handled them in a proper manner. The participants were asked to fill out feedback forms that were supplied to them via WhatsApp.</p>
 
- 
-<p>The workshop thus came to an end.</p>
-        `,
-        fullDate:"20/06/2021",
-        date:"26",
-        month:"20/06/2021"
-    },
-    {
-        id:"2",
-        imgSrc:"assets/images/event/pic2.jpg",
-        title:"2Education Autumn Tour 2019",
-        content:"2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
-        fullDate:"20/06/2021",
-        date:"26",
-        month:"20/06/2021"
-    },
-    {
-        id:"3",
-        imgSrc:"assets/images/event/pic3.jpg",
-        title:"2Education Autumn Tour 2019",
-        content:"2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since thn",
-        fullDate:"20/06/2021",
-        date:"26",
-        month:"20/06/2021"
-    }
 ]
 
 
+url="https://api-eu-central-1.graphcms.com/v2/ckpuxxcyxahs501wk7lu1epam/master?query=%7B%0A%20%20events%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20blog%20%7B%0A%20%20%20%20%20%20raw%0A%20%20%20%20%20%20html%0A%20%20%20%20%20%20markdown%0A%20%20%20%20%20%20text%0A%20%20%20%20%7D%0A%20%20%20%20images%20%7B%0A%20%20%20%20%20%20url%0A%20%20%20%20%20%20handle%0A%20%20%20%20%20%20fileName%0A%20%20%20%20%20%20height%0A%20%20%20%20%20%20width%0A%20%20%20%20%20%20size%0A%20%20%20%20%20%20mimeType%0A%20%20%20%20%7D%0A%20%20%20%20slug%0A%20%20%20%20thumbnail%20%7B%0A%20%20%20%20%20%20url%0A%20%20%20%20%20%20handle%0A%20%20%20%20%20%20fileName%0A%20%20%20%20%20%20height%0A%20%20%20%20%20%20width%0A%20%20%20%20%20%20size%0A%20%20%20%20%20%20mimeType%0A%20%20%20%20%7D%0A%20%20%20%20date%20%0A%20%20%20%20month_name%0A%20%20%20%20date_number%0A%20%20%7D%0A%7D"
+
+async function getEventDataFromServer(){
+    const reps = await fetch(url);
+    var data = await reps.json();
+    data= (data.data.events);
+    var curr=""
+    for(i=0;i<data.length;i++){
+        curr=data[i]
+       temp={
+           
+           blogid:curr.id,
+           content:curr.blog.html,
+           title:curr.title,
+           imgSrc:"assets/images/event/flashposter.jpg",
+           slug:curr.slug,
+           fullDate:curr.date,
+           date:curr.date_number,
+           month:curr.month_name
+        
+        }
+        eventData.push(temp)
+        // console.log(curr,temp)
+    }
+   
+
+    console.log(eventData,curr)
+}
+getEventDataFromServer()
