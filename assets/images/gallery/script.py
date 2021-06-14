6d9155a483dcc4026dd5bdadbb15c55a7f727607
Grejo Joby <grejo00@gmail.com>
2021-06-14 14:37:13 +0530

    images extra remove, footer changed

--- assets/images/gallery/script.py
+++ assets/images/gallery/script.py
@@ -1,4 +1,8 @@
 import os
+
+fileList = []
 for subdir, dirs, files in os.walk('./'):
     for file in files:
-      print(file)
+      fileList.append(file)
+fileList.remove('script.py')
+print(fileList)
