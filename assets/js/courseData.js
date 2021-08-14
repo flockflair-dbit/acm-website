0b7df22ccc8e7e2c7ad5f7bfc98c7af808092e53
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-08-14 19:40:13 +0530

    updated courses

--- assets/js/courseData.js
+++ assets/js/courseData.js
@@ -1,8 +1,7 @@
 courseData=[
 
 ]
-
-url="https://api-eu-central-1.graphcms.com/v2/ckpv3up06dqcq01xxfy3y5xwn/master?query=%7B%0A%20%20courses(orderBy%3AeventDate_DESC)%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20youtubeID%0A%20%20%20%20taughtBy%0A%20%20%20%20date%0A%20%20%20%20eventDate%0A%20%20%20%20%0A%20%20%20%20%0A%20%20%7D%0A%7D"
+url="https://api-eu-central-1.graphcms.com/v2/ckpv3up06dqcq01xxfy3y5xwn/master?query=%7B%0A%20%20courses(orderBy%3AeventDate_DESC)%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20youtubeID%0A%20%20%20%20taughtBy%0A%20%20%20%20eventDate%0A%20%20%20%20%0A%20%20%20%20%0A%20%20%7D%0A%7D"
 async function getEventDataFromServer(){
     const reps = await fetch(url);
     var data = await reps.json();
@@ -11,9 +10,14 @@ async function getEventDataFromServer(){
    
     for(var i=0;i<data.length;i++){
         courseData.push(data[i])
+
+      
+
+        courseData[i]["eventDate"]=(courseData[i]["eventDate"].replaceAll("-", "/"));
+
     }
     // courseData.reverse();
-    // console.log(courseData)
+    console.log(courseData)
 }
 getEventDataFromServer()
 
