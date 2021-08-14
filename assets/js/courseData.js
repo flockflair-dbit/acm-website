83cca50c3cf0278e5c00fc7375c870007d782626
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-08-14 19:33:31 +0530

    sorted events according to date

--- assets/js/courseData.js
+++ assets/js/courseData.js
@@ -2,7 +2,7 @@ courseData=[
 
 ]
 
-url="https://api-eu-central-1.graphcms.com/v2/ckpv3up06dqcq01xxfy3y5xwn/master?query=query%7B%0A%20%20courses%7B%0A%20%20%20%20title%2C%0A%20%20%20%20taughtBy%2C%0A%20%20%20%20date%2C%0A%20%20%20%20youtubeID%0A%20%20%7D%0A%7D"
+url="https://api-eu-central-1.graphcms.com/v2/ckpv3up06dqcq01xxfy3y5xwn/master?query=%7B%0A%20%20courses(orderBy%3AeventDate_DESC)%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20youtubeID%0A%20%20%20%20taughtBy%0A%20%20%20%20date%0A%20%20%20%20eventDate%0A%20%20%20%20%0A%20%20%20%20%0A%20%20%7D%0A%7D"
 async function getEventDataFromServer(){
     const reps = await fetch(url);
     var data = await reps.json();
@@ -12,8 +12,8 @@ async function getEventDataFromServer(){
     for(var i=0;i<data.length;i++){
         courseData.push(data[i])
     }
-    // reportData.reverse();
-    console.log(courseData)
+    // courseData.reverse();
+    // console.log(courseData)
 }
 getEventDataFromServer()
 
