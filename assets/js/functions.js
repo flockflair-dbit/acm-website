cb12367bf534056a53cbd675608bbb113f686602
Grejo Joby <grejo00@gmail.com>
2021-08-15 15:06:36 +0530

    code clean

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -31,64 +31,6 @@
 			});
 		}
 
-		var setUpcomingEventCarousel = function () {
-			if (!checkSelectorExistence('.upcoming-event-carousel')) { return; }
-			jQuery('.upcoming-event-carousel').owlCarousel({
-				center: true,
-				autoplay: true,
-				items: 3,
-				loop: true,
-				margin: 0,
-				nav: false,
-				dots: true,
-				autoplaySpeed: 1000,
-				navSpeed: 1000,
-				paginationSpeed: 1000,
-				slideSpeed: 1000,
-				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-				responsive: {
-					0: {
-						items: 1
-					},
-					650: {
-						items: 1.5
-					},
-					1024: {
-						items: 2
-					},
-					1200: {
-						items: 2
-					}
-				}
-			});
-		}
-
-		var setRecentNewsCarousel = function () {
-			if (!checkSelectorExistence('.recent-news-carousel')) { return; }
-			jQuery('.recent-news-carousel').owlCarousel({
-				loop: true,
-				autoplay: true,
-				margin: 30,
-				nav: false,
-				dots: true,
-				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-				responsive: {
-					0: {
-						items: 1
-					},
-					480: {
-						items: 2
-					},
-					1024: {
-						items: 3
-					},
-					1200: {
-						items: 3
-					}
-				}
-			});
-		}
-
 		var setTestimonialCarousel = function () {
 			if (!checkSelectorExistence('.testimonial-carousel')) { return; }
 			jQuery('.testimonial-carousel').owlCarousel({
@@ -119,8 +61,6 @@
 		return {
 			initialHelper: function () {
 				setCourseCarousel();
-				setUpcomingEventCarousel();
-				setRecentNewsCarousel();
 				setTestimonialCarousel();
 			}
 		}
