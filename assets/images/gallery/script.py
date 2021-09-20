d77d4cddff3b9a835b14c8f9eb50739286d9fb41
Grejo Joby <grejo00@gmail.com>
2021-09-20 22:24:17 +0530

    sorted gallery

--- assets/images/gallery/script.py
+++ assets/images/gallery/script.py
@@ -4,5 +4,7 @@ fileList = []
 for subdir, dirs, files in os.walk('./'):
     for file in files:
       fileList.append(file)
+
+fileList.sort(key=os.path.getctime, reverse=True)
 fileList.remove('script.py')
 print(fileList)
