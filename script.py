4c5491cfe9c1d8052cba7cebf15e45f7f602b54b
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-11-03 20:40:37 +0530

    updated path script

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ def create_gallery_images_js():
       for file in files:
         fileList.append(file)
 
-  # fileList.sort(key=os.path.getctime, reverse=True)
+  fileList.sort(key=lambda x: os.path.getctime('./assets/images/gallery/'+x), reverse=True)
   file1 = open("./assets/js/galleryData.js","w+")
   file1.write("var galleryList = [")
   for i in fileList:
