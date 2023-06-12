a5ae38a67672c6e7f75f0213b28ccbd2e1038132
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-06-12 13:41:19 +0530

    made exhibition static

--- src/utils/cmsData.ts
+++ src/utils/cmsData.ts
@@ -1,5 +1,6 @@
 import type { CourseData } from "../components/course/type"
 import type { report } from "../react-pages/reports/types"
+import type { projectData } from "../react-pages/e-xhibition/types"
 
 // This function fetches the courses from GraphCMS
 export const getCourses = async (): Promise<CourseData[]> => {
@@ -15,5 +16,13 @@ export const getReports = async () => {
     const res = await fetch(url)
     const json = await res.json()
     const data: report[] = json.data.reports
+    return data
+}
+
+export const getProjects = async () => {
+    const url = "https://api-eu-central-1.graphcms.com/v2/ckpv3up06dqcq01xxfy3y5xwn/master?query=%7B%0A%20%20projects(orderBy%3ApublishedAt_DESC)%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20developerName%0A%20%20%20%20videoLink%0A%20%20%20%20projectLink%0A%20%20%20%20publishedAt%0A%20%20%20%20slug%0A%20%20%7D%0A%7D"
+    const res = await fetch(url)
+    const json = await res.json()
+    const data: projectData[] = json.data.projects
     return data
 }
