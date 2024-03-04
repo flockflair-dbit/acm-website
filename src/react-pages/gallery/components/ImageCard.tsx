c163d7ea5d8a1ea9e23486421c11fcd4bac1b908
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-03-03 20:05:52 -0500

    migrated to react page

---
+++ src/react-pages/gallery/components/ImageCard.tsx
@@ -0,0 +1,24 @@
+
+const ImageCard: React.FC<{ image: string }> = ({image }) => {
+
+    return (
+<div className="action-card col-lg-3 col-md-4 col-sm-6 book">
+    <div className="ttr-box portfolio-bx">
+        <div className="ttr-media media-ov2 media-effect">
+            <a href="javascript:void(0);">
+                <img src={image} alt="" loading="lazy" />
+            </a>
+            <div className="ov-box">
+                <div className="overlay-icon align-m">
+                    <a href={image} className="magnific-anchor" title="">
+                        <i className="ti-search"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    )
+}
+
+export default ImageCard
