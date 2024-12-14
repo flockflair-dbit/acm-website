564ed12e9a94c2e7e56d81d4ba9168e72287ffbb
000Andre000 <89241490+000Andre000@users.noreply.github.com>
2024-12-14 17:58:09 +0530

    Calendar and Basic Chatbot

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 import Head from "./Head.astro";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Chatbot from "../../react-pages/chatbot/chatbot";
 
-export { Head, Navbar, Footer }
+export { Head, Chatbot, Navbar, Footer }
